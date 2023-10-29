066e2f4eee34c7ca6ebd84f8587393f78974c378
Himanshu Bisht <himanshubisht313@gmail.com>
2023-10-29 19:37:43 +0530

    select created in header

--- src/components/Board/Board.tsx
+++ src/components/Board/Board.tsx
@@ -1,4 +1,4 @@
-import { BiSignal3 } from "react-icons/bi";
+import { BiSignal3, BiPlus, BiDotsHorizontalRounded } from "react-icons/bi";
 
 import Card from "../Card/Card";
 import styles from "./Board.module.css";
@@ -7,10 +7,24 @@ export default function Board() {
   return (
     <div className={styles.board}>
       <div className={styles.header}>
-        <span>
-          <BiSignal3 />
-        </span>
-        <p>High 5</p>
+        <div className={styles.left}>
+          <span>
+            <BiSignal3 />
+          </span>
+          <div className={styles.boardInfo}>
+            <p>High</p>
+            <p>5</p>
+          </div>
+        </div>
+
+        <div className={styles.right}>
+          <button>
+            <BiPlus />
+          </button>
+          <button>
+            <BiDotsHorizontalRounded />
+          </button>
+        </div>
       </div>
       <div className={styles.cardsContainer}>
         <Card />
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,13 +1,54 @@
+import { BiSliderAlt, BiChevronDown } from "react-icons/bi";
+
 import styles from "./Header.module.css";
+import { useState } from "react";
 
 export default function Header() {
+  const [popupOpen, setPopupOpen] = useState(false);
+
   return (
     <header className={styles.header}>
-      <select>
-        <option>Display</option>
-        <option>User</option>
-        <option>Priority</option>
-      </select>
+      <div
+        className={styles.select}
+        onClick={() => setPopupOpen((prev) => !prev)}
+      >
+        <span>
+          <BiSliderAlt />
+        </span>
+        <div className={styles.content}>
+          <p>Settings</p>
+          <span>
+            <BiChevronDown />
+          </span>
+        </div>
+      </div>
+
+      <div
+        className={styles.popup}
+        style={
+          popupOpen
+            ? { opacity: 1, visibility: "visible" }
+            : { opacity: 0, visibility: "hidden" }
+        }
+      >
+        <div className={styles.popupItem}>
+          <label htmlFor="grouping">Grouping</label>
+          <select name="grouping">
+            <option>Status</option>
+            <option>Status</option>
+            <option>Status</option>
+          </select>
+        </div>
+
+        <div className={styles.popupItem}>
+          <label htmlFor="ordering">Ordering</label>
+          <select name="ordering">
+            <option>Priority</option>
+            <option>Priority</option>
+            <option>Priority</option>
+          </select>
+        </div>
+      </div>
     </header>
   );
 }
