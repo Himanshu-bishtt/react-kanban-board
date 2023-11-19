6ffe3708dcfb35961892b9de794d166123122114
Himanshu Bisht <himanshubisht313@gmail.com>
2023-11-19 12:18:02 +0530

    reading file from the backend

--- server/app.js
+++ server/app.js
@@ -1,9 +1,15 @@
+import fs from "fs";
 import express from "express";
 import cors from "cors";
+
+const tickets = fs.readFileSync("data/tickets.json", "utf-8");
+const users = fs.readFileSync("data/users.json", "utf-8");
 
 const app = express();
 
 app.use(cors());
+
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.status(200).json({ status: "success" });
