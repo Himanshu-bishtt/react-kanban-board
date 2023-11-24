1cd9e4659e70706e2f2b4cbbd525196accd5737b
Himanshu Bisht <himanshubisht313@gmail.com>
2023-11-24 23:13:26 +0530

    using mongoose for api

--- server/controllers/ticketsController.js
+++ server/controllers/ticketsController.js
@@ -1,146 +1,89 @@
-import fs from "fs";
-import { users } from "../app.js";
+import Ticket from "../models/ticketModal.js";
 
-const tickets = JSON.parse(fs.readFileSync("data/tickets.json", "utf-8"));
-
-export function getTickets(req, res) {
-  res.status(200).json({
-    status: "success",
-    results: tickets.length,
-    data: {
-      tickets,
-    },
-  });
-}
-
-export function getTicket(req, res) {
-  const ticketId = `CAM-${req.params.id}`;
-
-  const ticket = tickets.find((t) => t.id === ticketId);
-
-  if (!ticket)
-    return res.status(404).json({
-      status: "fail",
-      message: "Invalid id",
-    });
-
-  res.status(200).json({
-    status: "success",
-    data: {
-      ticket,
-    },
-  });
-}
-
-export function createTicket(req, res) {
-  const { title, tag, user, status, priority } = req.body;
-
-  const newId = Number(tickets.at(-1).id.split("-").at(1)) + 1;
-
-  const newTags = tag.split(", ");
-
-  const newUserId = users.find(
-    (u) => u.name.toLowerCase() === user.toLowerCase()
-  ).id;
-
-  const newTicket = {
-    id: `CAM-${newId}`,
-    title,
-    tag: newTags,
-    userId: newUserId,
-    status,
-    priority,
-  };
-
-  tickets.push(newTicket);
-
-  fs.writeFile("data/tickets.json", JSON.stringify(tickets), (err) => {
-    if (err) {
-      return res.status(409).json({
-        status: "fail",
-        message: "Internal server error",
-      });
-    }
-    res.status(201).json({
+export async function getTickets(req, res) {
+  try {
+    const tickets = await Ticket.find();
+    res.status(200).json({
       status: "success",
       results: tickets.length,
       data: {
         tickets,
       },
     });
-  });
+  } catch (err) {
+    res.status(404).json({
+      status: "fail",
+      message: err.message,
+    });
+  }
 }
 
-export function updateTicket(req, res) {
-  const id = `CAM-${req.params.id}`;
-
-  if (Object.keys(req.body).length === 0) {
-    return res
-      .status(400)
-      .json({ status: "fail", message: "Request body cannot be empty" });
-  }
-
-  const ticket = tickets.find((t) => t.id === id);
-  const ticketIndex = tickets.findIndex((t) => t.id === id);
-
-  const newTag = req.body?.tag?.split(", ") || ticket.tag;
-
-  let newUserId = ticket.userId;
-  if (req.body.user) {
-    newUserId =
-      users.find((u) => u.name.toLowerCase() === req.body.user.toLowerCase())
-        .id || ticket.userId;
-  }
-
-  const newTitle = req.body?.title || ticket.title;
-  const newStatus = req.body?.status || ticket.status;
-  const newPriority = req.body?.priority || ticket.priority;
-
-  const updatedTicket = {
-    ...ticket,
-    title: newTitle,
-    tag: newTag,
-    userId: newUserId,
-    status: newStatus,
-    priority: newPriority,
-  };
-
-  tickets.splice(ticketIndex, 1, updatedTicket);
-
-  fs.writeFile("data/tickets.json", JSON.stringify(tickets), (err) => {
-    if (err) {
-      return res.status(500).json({
-        status: "fail",
-        message: "Internal server error",
-      });
-    }
+export async function getTicket(req, res) {
+  try {
+    const ticket = await Ticket.findById(req.params.id);
     res.status(200).json({
       status: "success",
       data: {
-        ticket: updatedTicket,
+        ticket,
       },
     });
-  });
+  } catch (err) {
+    res.status(404).json({
+      status: "fail",
+      message: "Invalid ID",
+    });
+  }
 }
 
-export function deleteTicket(req, res) {
-  const id = `CAM-${req.params.id}`;
+export async function createTicket(req, res) {
+  try {
+    const tag = req.body.tag.split(", ");
+    const newTicket = await Ticket.create({ ...req.body, tag });
+    res.status(201).json({ status: "success", data: { ticket: newTicket } });
+  } catch (err) {
+    res.status(409).json({
+      status: "fail",
+      message: err.message,
+    });
+  }
+}
 
-  const updatedTickets = tickets.filter((t) => t.id !== id);
+export async function updateTicket(req, res) {
+  try {
+    if (Object.keys(req.body).length === 0)
+      throw new Error("Body cannot be empty");
 
-  fs.writeFile("data/tickets.json", JSON.stringify(updatedTickets), (err) => {
-    if (err) {
-      return res.status(500).json({
-        status: "fail",
-        message: "Internal server error",
-      });
-    }
+    const ticket = await Ticket.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true,
+    });
+
     res.status(200).json({
       status: "success",
-      results: updatedTickets.length,
       data: {
-        tickets: updatedTickets,
+        ticket,
       },
     });
-  });
+  } catch (err) {
+    res.status(400).json({
+      status: "fail",
+      message: err.message,
+    });
+  }
+}
+
+export async function deleteTicket(req, res) {
+  try {
+    const ticket = await Ticket.findByIdAndDelete(req.params.id);
+    if (!ticket) throw new Error("Ticket does not exist");
+    res.status(204).json({
+      status: "success",
+      data: null,
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: "fail",
+      message: err.message,
+    });
+  }
 }
